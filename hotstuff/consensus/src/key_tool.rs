85d974e0a5cf2ec89dd4984fa4324ee63218d039
Ljiacheng <Ljiachenggh@gmail.com>
2025-11-05 11:28:10 +0800

    hotstuff hots_key tool for path derive

--- hotstuff/consensus/src/key_tool.rs
+++ hotstuff/consensus/src/key_tool.rs
@@ -1,7 +1,6 @@
 use std::env;
-use codec::Encode;
-use w3f_bls::{DoublePublicKeyScheme, Keypair, SerializableToBytes, TinyBLS381};
 use sp_core::{ByteArray, Pair};
+use sp_core::crypto::{ExposeSecret, SecretStringError, SecretUri};
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
@@ -9,23 +8,59 @@ fn main() {
         panic!("invalid params");
     }
     match args[1].as_str() {
-        "bls381" => generate_bls_381(),
+        "bls381" => if args.len() == 2 {
+            generate_bls_381();
+        } else {
+            from_string_seed(args);
+        },
         _ => panic!("invalid mission"),
     }
 }
 
+
 fn generate_bls_381() {
     use sp_application_crypto::bls381;
     let (pair, seed) = bls381::AppPair::generate();
-    // let secret = w3f_bls::SecretKey::<TinyBLS381>::from_seed(seed.as_slice());
-    // let keypair = Keypair {
-    //     public: secret.into_public(),
-    //     secret,
-    // };
-
     println!("seed:\n0x{}", hex::encode(seed.as_slice()));
-    // println!("secret:\n0x{}", hex::encode(keypair.secret.to_bytes()));
     println!("pubkey:\n0x{}", hex::encode(pair.public().as_slice()));
-    // let double_pk = DoublePublicKeyScheme::into_double_public_key(&keypair).to_bytes();
-    // println!("double_pubkey:\n0x{}", hex::encode(double_pk));
+}
+
+fn from_string_seed(args: Vec<String>) {
+    use hotstuff_primitives::bls_crypto::Pair as BLS381Pair;
+    use std::str::FromStr;
+
+    let s = args[2].as_str();
+    let mut password_override = None;
+    if args.len() >= 4 {
+        password_override = Some(args[3].as_str());
+    }
+
+    let SecretUri { junctions, phrase, password } = SecretUri::from_str(s).unwrap();
+    let password =
+        password_override.or_else(|| password.as_ref().map(|p| p.expose_secret().as_str()));
+
+    let (root, seed) = if let Some(stripped) = phrase.expose_secret().strip_prefix("0x") {
+        array_bytes::hex2bytes(stripped)
+            .ok()
+            .and_then(|seed_vec| {
+                let mut seed = <hotstuff_primitives::bls_crypto::Pair as Pair>::Seed::default();
+                if seed.as_ref().len() == seed_vec.len() {
+                    seed.as_mut().copy_from_slice(&seed_vec);
+                    Some((BLS381Pair::from_seed(&seed), seed))
+                } else {
+                    None
+                }
+            })
+            .ok_or(SecretStringError::InvalidSeed)
+            .unwrap()
+    } else {
+        BLS381Pair::from_phrase(phrase.expose_secret().as_str(), password)
+            .map_err(|_| SecretStringError::InvalidPhrase)
+            .expect("invalid phrase, if need derive, please add \"//\" as prefix.")
+    };
+    let (pair, seed) = root.derive(junctions.into_iter(), Some(seed))
+        .map_err(|_| SecretStringError::InvalidPath).unwrap();
+
+    println!("seed:\n0x{}", hex::encode(seed.unwrap().as_slice()));
+    println!("pubkey:\n0x{}", hex::encode(pair.public().as_slice()));
 }
